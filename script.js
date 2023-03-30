6ea99213f0ec41a0906a31d55d8fa0af2429e71c
Matt Hickling <m.hickling@hotmail.co.uk>
2023-03-30 08:14:57 +0100

    removed initial task

--- script.js
+++ script.js
@@ -7,13 +7,11 @@ window.addEventListener('load', () => {
 		e.preventDefault();
 
 		const task = input.value;
-
 		const task_el = document.createElement('div');
 		task_el.classList.add('task');
 
 		const task_content_el = document.createElement('div');
 		task_content_el.classList.add('content');
-
 		task_el.appendChild(task_content_el);
 
 		const task_input_el = document.createElement('input');
@@ -21,20 +19,19 @@ window.addEventListener('load', () => {
 		task_input_el.type = 'text';
 		task_input_el.value = task;
 		task_input_el.setAttribute('readonly', 'readonly');
-
 		task_content_el.appendChild(task_input_el);
 
 		const task_actions_el = document.createElement('div');
 		task_actions_el.classList.add('actions');
-		
+		// ----------Edit
 		const task_edit_el = document.createElement('button');
 		task_edit_el.classList.add('edit');
 		task_edit_el.innerText = 'Edit';
 
+		//----------Delete
 		const task_delete_el = document.createElement('button');
 		task_delete_el.classList.add('delete');
 		task_delete_el.innerText = 'Delete';
-
 		task_actions_el.appendChild(task_edit_el);
 		task_actions_el.appendChild(task_delete_el);
 
@@ -43,7 +40,7 @@ window.addEventListener('load', () => {
 		list_el.appendChild(task_el);
 
 		input.value = '';
-
+		
 		task_edit_el.addEventListener('click', (e) => {
 			if (task_edit_el.innerText.toLowerCase() == "edit") {
 				task_edit_el.innerText = "Save";
